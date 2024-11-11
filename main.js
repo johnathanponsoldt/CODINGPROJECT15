f4b5e03b28d18c2359f183781466d37c666c1dee
johnathanponsoldt <jponsoldt772@gmail.com>
2024-11-10 23:22:27 -0500

    Implement main application and log portfolio details and transactions.

---
+++ main.js
@@ -0,0 +1,9 @@
+import { calculatePortfolioValue, getPortfolioAllocation} from 'portfolio.js';
+
+import Transaction from 'transaction.js';
+
+// Create a new order
+const Transaction = new Transaction(1);
+order.addItem(1, 2);  // 3 Footballs
+order.addItem(3, 1);  // 1 Basketball
+
--- portfolio.js
+++ portfolio.js
@@ -3,3 +3,4 @@ import { assets } from './asset.js';
 export function calculatePortfolioValue() {
     return assets.reduce((total, asset) => total + asset.price * asset.quantity, 0);
 }
+
